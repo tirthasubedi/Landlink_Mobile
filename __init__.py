7c1089e72f0c004fc0e42f693343d16ffe58864c
Community Farm Alliance <cfa@tango-server.berea.edu>
2018-11-29 23:02:02 -0500

    Added to creation of account with database

--- __init__.py
+++ __init__.py
@@ -42,8 +42,23 @@ def confirmedSent():
 def accountCreated():
     conn,cur = connect()
 
-    return render_template("accountCreated.html")
-    #return request.form['Email']
+    #return render_template("accountCreated.html")
+    data = {}
+    data['Email'] = request.form.get('Email')
+    data['Password'] = request.form.get('Password')
+    data['Name'] = request.form.get('Name')
+    data['Address'] = request.form.get('Address')
+    data['State'] = request.form.get('State')
+    data['City'] = request.form.get('City')
+    data['ZipCode'] = int(request.form.get('ZipCode'))
+    data['Phone'] = int(request.form.get('Phone'))
+    data['isFarmer'] = int(request.form.get('isFarmer'))
+
+    query = "INSERT INTO `Applicants`(`Name`, `Email`, `Phone`, `Password`, `isFarmer`, `Address`, `State`, `City`, `ZipCode`, `ApplicantID`) VALUES ('{0}','{1}',{2},'{3}',{4},'{5}','{6}','{7}',{8},NULL)".format(data['Name'],data['Email'],data['Phone'],data['Password'],data['isFarmer'],data['Address'],data['State'],data['City'],data['ZipCode'])
+    cur.execute(query)
+    conn.commit()
+    conn.close()    
+    return render_template('accountCreated.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
